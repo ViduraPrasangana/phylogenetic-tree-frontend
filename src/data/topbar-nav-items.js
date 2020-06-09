b278447d872b2bba63ad9fc9e0796d63e1faabf9
Vidura Prasangana <vidura.prasangana16@gmail.com>
2020-06-09 20:59:59 +0530

    deleted some

--- src/data/topbar-nav-items.js
+++ src/data/topbar-nav-items.js
@@ -28,25 +28,5 @@
     protect:true
   },
 ]
-  const itemsLeft = [
-    {
-      title: "Search flights",
-      to: "/",
-      htmlBefore: '<i class="fas fa-plane" style="fontSize: 13.5em"></i>',
-      htmlAfter: ""
-    },
-    {
-      title: "My Tickets",
-      to: "/tickets",
-      htmlBefore: '<i class="fas fa-plus"></i>',
-      htmlAfter: ""
-    },
-    {
-      title: "My Bookings",
-      to: "/bookings",
-      htmlBefore: '<i class="fas fa-users"></i>',
-      htmlAfter: ""
-    }
   
-];
-export  {itemsLeft,itemsRight}
+export  {itemsRight}
